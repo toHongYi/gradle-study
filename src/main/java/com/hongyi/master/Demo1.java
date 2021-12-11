b559680e7d94fc8609216ee76a2261258e7ca409
hongyi <the_hongyi@163.com>
2021-12-12 00:19:12 +0800

    Initial commit

---
+++ src/main/java/com/hongyi/master/Demo1.java
@@ -0,0 +1,13 @@
+package com.hongyi.master;
+
+/**
+ * @version : V1.0
+ * @date : 2021/12/12 0:15
+ * @Author : LLH
+ * @Desc :
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("master");
+    }
+}
